2e707546beabc66acce885b3ad3db3d8193cda7b
M A RAHMAN <67161019+abdurrahmang6156@users.noreply.github.com>
2020-11-27 11:47:50 -0700

    o

---
+++ My JavaScript.js
@@ -0,0 +1,34 @@
+
+alert("Hi tu");
+
+console.log("Javascript is a easy scripting language.");
+console.log('wordpress is the best app of webdevelopment');
+console.log(" Go there");
+console.log('come here');
+console.log('don`t go there.');
+console.log('one day or other.');
+console.log('I am intetested.');
+console.log('I am bored.');
+console.log('I am realistic.');
+console.log('I am imaginative.');
+console.log('I am healthy.');
+console.log('I am sick.');
+
+
+
+
+document.write(" This is document.");
+document.write('In the name of ALLAH I begin');
+document.write('Your best teacher is your last mistake.');
+document.write('I am satisfied.');
+document.write('I am disappointed.');
+document.write('I am cheerful.');
+document.write('I am gloomy.');
+document.write('I am optimistic.');
+document.write('I am hopeless.');
+
+
+
+document.getElementById('id1').innerHTML = "Bangladesh";
+document.getElementById('id2').innerHTML = 'I am lively';
+document.getElementById('id3').innerHTML = 'I am tired';
